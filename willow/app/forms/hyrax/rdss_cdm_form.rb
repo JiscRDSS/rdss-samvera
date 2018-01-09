702d935d7163c5e01a1a7ebc6f9d03183547c169
Daniel Berzon <daniel@ocasta.com>
2018-01-09 14:54:44 +0000

    Changes to the RdssCdm form object to allow a nested date property

--- willow/app/forms/hyrax/rdss_cdm_form.rb
+++ willow/app/forms/hyrax/rdss_cdm_form.rb
@@ -9,10 +9,31 @@ module Hyrax
       :object_description,
       :object_keywords,
       :object_category,
-      :object_version
+      :object_version,
+      :object_date
     ]
     self.required_fields = [
       :title
     ]
+
+    NESTED_ASSOCIATIONS = [:object_date]
+    # Permitted parameters for nested attributes
+    
+    def self.permitted_object_date_params
+      [:id,
+       :_destroy,
+       {
+         date_value: [],
+         date_type: []
+       },
+      ]
+    end
+
+    def self.build_permitted_params
+        permitted = super
+        # add in object_date attributes
+        permitted << { object_date:  permitted_object_date_params}        
+        permitted
+      end
   end
 end
