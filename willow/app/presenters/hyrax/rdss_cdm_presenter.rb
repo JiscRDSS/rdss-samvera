aa2b28ab58b11ba8c2a07966616c324241cfce7f
Daniel Berzon <daniel@ocasta.com>
2018-01-12 15:52:07 +0000

    Changes to be able to display object_date on the show view for RdssCdm

--- willow/app/presenters/hyrax/rdss_cdm_presenter.rb
+++ willow/app/presenters/hyrax/rdss_cdm_presenter.rb
@@ -3,5 +3,20 @@
 module Hyrax
   class RdssCdmPresenter < Hyrax::WorkShowPresenter
     delegate :title, :object_description, :object_keywords, :object_category, to: :solr_document
+    
+    # translate the json string for object date into a displayable value
+    def object_date
+      values = []
+      date = solr_document.object_date.try(:first)
+      if date
+        date_json = JSON.parse date
+        date_json.each do |date|
+          type = date['date_type'].try(:first)
+          value = date['date_value'].try(:first)
+          values << "#{type.humanize}: #{value}" unless(type.blank? || value.blank?)
+        end
+      end
+      values
+    end
   end
 end
