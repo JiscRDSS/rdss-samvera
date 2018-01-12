c8798d6afee524bf175c50d7e1281b7ed7605afd
Daniel Berzon <daniel@ocasta.com>
2018-01-12 15:50:50 +0000

    Adding in the initializer functionality from cottage labs
    but added default parameters to prevent the intermittent rails error on dev

--- willow/app/models/cdm/date.rb
+++ willow/app/models/cdm/date.rb
@@ -5,5 +5,19 @@ class Cdm::Date < ActiveTriples::Resource
   configure type: ::RDF::Vocab::VCARD.Date
   property :date_value, predicate: ::RDF::Vocab::DC.date
   property :date_type, predicate: ::RDF::Vocab::DC.description
+
+  ## Necessary to get AT to create hash URIs.
+  # Added in the initializer from cottaglabs, as it seems to prevent errors when entering the date into fedora
+  # However I've added in default values for the parameters so it is possible to call Cdm::Date.new with no parameters.
+  def initialize(uri=nil, parent=nil)
+    if uri
+      if uri.try(:node?)
+        uri = RDF::URI("#object_date#{uri.to_s.gsub('_:', '')}")
+      elsif uri.start_with?("#")
+        uri = RDF::URI(uri)
+      end
+    end
+    super
+  end
   
 end
