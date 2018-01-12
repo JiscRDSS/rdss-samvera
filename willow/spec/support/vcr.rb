a39de46731c5e78632cc3456e89d76cb8904053a
Paul McKibbin <pmckibbin@gmail.com>
2018-01-12 09:47:36 +0000

    Revert "Feature/rdsssam 132 create new basic samvera rdsscdm work type"

--- willow/spec/support/vcr.rb
+++ willow/spec/support/vcr.rb
@@ -2,6 +2,8 @@ require 'active_support/core_ext/numeric/time'
 require 'vcr'
 require 'webmock/rspec'
 
+
+
 VCR.configure do |config|
   #the directory where your cassettes will be saved
   config.cassette_library_dir = 'spec/fixtures/vcr'
@@ -9,7 +11,6 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.default_cassette_options = { match_requests_on: [:method, :uri],
                                       record: ENV["VCR_RECORD_MODE"] ? ENV["VCR_RECORD_MODE"].to_sym : :once }
-
   config.configure_rspec_metadata!
 end
 
