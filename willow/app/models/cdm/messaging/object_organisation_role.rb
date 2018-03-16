e9dd099a07cbcc4034ba74260c496c51a51c7083
Finlay McCourt <finlay.mccourt@digirati.com>
2018-03-16 15:29:41 +0000

    Adding a default ObjectOrganisationRole containing information about the HEI environment generating the message.

--- willow/app/models/cdm/messaging/object_organisation_role.rb
+++ willow/app/models/cdm/messaging/object_organisation_role.rb
@@ -16,14 +16,28 @@ module Cdm
         def map_organisation_role(mapping, object)
           object.send(attribute_name_in_model).map do |oor|
             ::Cdm::Messaging::Organisation.(:organisation, mapping.first['organisation'], oor.organisation).merge({ role: ::Cdm::Messaging::Enumerations::OrganisationRole.send(oor.role) })
-          end.flatten
+          end.flatten.unshift(default_messaging_oor)
+        end
+
+        # As some components of the RDSS are using this field to determine the institution sending the message to inform their processing of the message, 
+        # the following default oor populated with information about the HEI cluster is prepended to the list to support this, until such time as it is
+        # no longer required. 
+        def default_messaging_oor
+          {
+            organisation: {
+              organisationJiscId: Willow::Config.institution_jisc_id,
+              organisationName: Willow::Config.institution_name,
+              organisationType: ::Cdm::Messaging::Enumerations::OrganisationType.higher_education,
+              organisationAddress: Willow::Config.institution_name
+            }, 
+            role: ::Cdm::Messaging::Enumerations::OrganisationRole.hosting_institution
+          }
         end
 
         def call(name, mapping, object)
           { name.intern=>map_organisation_role(mapping, object) }
         end
       end
-
     end
   end
 end
