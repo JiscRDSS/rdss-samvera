0ae1c6fb8b71a955a62eda828afa7d7d46aba138
Paul McKibbin <pmckibbin@gmail.com>
2018-03-21 17:31:02 +0000

    Minor clean-up of hyrax code.

--- willow/app/models/hyrax/contact_form.rb
+++ willow/app/models/hyrax/contact_form.rb
@@ -4,8 +4,21 @@ module Hyrax
     attr_accessor :contact_method, :category, :name, :email, :subject, :message
     validates :email, :category, :name, :subject, :message, presence: true
     validates :email, format: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i, allow_blank: true
-
     # - can't use this without ActiveRecord::Base validates_inclusion_of :category, in: self.class.issue_types_for_locale
+
+    class << self
+      private
+      def issue_types
+        %w(depositing changing browsing reporting general)
+      end
+
+      public
+      # TODO: Really this should be in a decorator rather than the model. Slightly sloppy here, but as this is an override
+      # of the Hyrax gem code, it's required to be in the model class methods. *Sigh*
+      def issue_types_for_locale
+        issue_types.map{|issue_type| I18n.t("hyrax.contact_form.issue_types.#{issue_type}")}
+      end
+    end
 
     # They should not have filled out the `contact_method' field. That's there to prevent spam.
     def spam?
@@ -28,16 +41,6 @@ module Hyrax
           # The message body (see the contact_mailer view in Hyrax) still contains the user's email address for admins to see.
           from: ENV['CONTACT_FORM_SENDER_EMAIL'] || Hyrax.config.contact_email  # use `email` as the value instead to try to send on behalf of the user
       }
-    end
-    
-    def self.issue_types_for_locale
-      [
-          I18n.t('hyrax.contact_form.issue_types.depositing'),
-          I18n.t('hyrax.contact_form.issue_types.changing'),
-          I18n.t('hyrax.contact_form.issue_types.browsing'),
-          I18n.t('hyrax.contact_form.issue_types.reporting'),
-          I18n.t('hyrax.contact_form.issue_types.general')
-      ]
     end
   end
 end
