0562e05d4a33f1e3722f79e17bcb44b95f56a3cb
Stewart Campbell <stewartcuk@gmail.com>
2018-03-21 12:41:19 +0000

    Coverage for object dates

---
+++ willow/spec/renderers/object_dates_attribute_render_spec.rb
@@ -0,0 +1,27 @@
+# 
+require 'spec_helper'
+
+RSpec.describe ObjectDatesAttributeRenderer do
+  describe 'Object Dates Attribute Renderer' do
+    let(:value) do
+      [
+        {
+          id: "84fabbb4-bdc2-4f73-a874-bb88faffbce9",
+          date_value: "07/03/2018",
+          date_type: "accepted",
+          rdss_cdm_id: "sx61dm28w"
+        }
+      ].to_json
+    end
+
+    subject {  Nokogiri::HTML(described_class.new(:object_dates, value).render) }
+
+    it 'looks up and displays the label' do
+      expect(subject.css('.td')[0].text).to eq('Accepted')
+    end
+
+    it 'displays the identifier value' do
+      expect(subject.css('.td')[1].text).to eq('07/03/2018')
+    end
+  end
+end
