5c0dbced223c92dc8fa387bad3faf7e958c11b88
Paul McKibbin <pmckibbin@gmail.com>
2018-04-04 00:35:24 +0100

    RDSSSAM-275 Update messaging to use v3.0.0 of Messaging API specification

--- willow/app/forms/hyrax/rdss_cdm_form.rb
+++ willow/app/forms/hyrax/rdss_cdm_form.rb
@@ -15,7 +15,7 @@ module Hyrax
       :object_value,
       :object_people,
       :object_dates,
-      :object_right,
+      :object_rights,
       :object_organisation_roles,
       :object_identifiers,
       :object_related_identifiers,
@@ -28,7 +28,7 @@ module Hyrax
       :object_value,
       :object_people,
       :object_dates,
-      :object_right,
+      :object_rights,
       :object_organisation_roles,
       :object_identifiers,
     ]
