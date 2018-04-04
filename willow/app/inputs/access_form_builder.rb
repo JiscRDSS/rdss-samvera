a39abad54a52381bab5b72392e6a1049f956a9ad
Paul McKibbin <pmckibbin@gmail.com>
2018-04-04 14:51:20 +0100

    RDSSSAM-276 Update forms to support v3.0.0 of CDM required and not-required flags (#153)
    * RDSSSAM-276 Update forms to support v3.0.0 of CDM required and not-required flags
    
    * RDSSSAM-276 Update forms to support v3.0.0 of CDM required and not-required flags
    
    * Fix missing honorific_suffix attribute from the test dummy.
    
    * Tweak tests to check against the correct string, id and name
    
    * Make sure access_statement isn't required.

--- willow/app/inputs/access_form_builder.rb
+++ willow/app/inputs/access_form_builder.rb
@@ -3,7 +3,7 @@ class AccessFormBuilder < RdssFields
     input :access_type, collection: Cdm::Selections::AccessType.call, prompt: :translate, required: required
   end
 
-  def access_statement(required: true)
+  def access_statement(required: false)
     input :access_statement, required: required
   end
 end
