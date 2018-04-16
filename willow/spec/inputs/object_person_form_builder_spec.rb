305484e79752ecb0a0017faac930d36b596b616a
Finlay McCourt <finlay.mccourt@digirati.com>
2018-04-16 17:00:30 +0100

    Updating spec to deal with new labels.

--- willow/spec/inputs/object_person_form_builder_spec.rb
+++ willow/spec/inputs/object_person_form_builder_spec.rb
@@ -6,27 +6,27 @@ end
 
 RSpec.describe ObjectPersonFormBuilder do
   context 'honorific_prefix' do
-    it_behaves_like 'labelled', FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).honorific_prefix, 'Honorific prefix', required: false
+    it_behaves_like 'labelled', FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).honorific_prefix, 'Title', required: false
     it_behaves_like 'optional',   FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).honorific_prefix
-    it_behaves_like 'labelled', FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).honorific_prefix(required: true), 'Honorific prefix', required: true
+    it_behaves_like 'labelled', FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).honorific_prefix(required: true), 'Title', required: true
     it_behaves_like 'required',   FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).honorific_prefix(required: true)
     it_behaves_like 'input',     FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).honorific_prefix
     it_behaves_like 'named',      FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).honorific_prefix, :honorific_prefix
   end
 
   context 'given_name' do
-    it_behaves_like 'labelled', FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).given_name, 'Given name', required: true
+    it_behaves_like 'labelled', FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).given_name, 'First name', required: true
     it_behaves_like 'required',   FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).given_name
-    it_behaves_like 'labelled', FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).given_name(required: false), 'Given name'
+    it_behaves_like 'labelled', FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).given_name(required: false), 'First name'
     it_behaves_like 'optional',   FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).given_name(required: false)
     it_behaves_like 'input',     FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).given_name
     it_behaves_like 'named',      FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).given_name, :given_name
   end
 
   context 'family_name' do
-    it_behaves_like 'labelled', FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).family_name, 'Family name', required: true
+    it_behaves_like 'labelled', FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).family_name, 'Last name', required: true
     it_behaves_like 'required',   FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).family_name
-    it_behaves_like 'labelled', FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).family_name(required: false), 'Family name'
+    it_behaves_like 'labelled', FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).family_name(required: false), 'Last name'
     it_behaves_like 'optional',   FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).family_name(required: false)
     it_behaves_like 'input',     FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).family_name
     it_behaves_like 'named',      FacetsProxy.(described_class,ObjectPersonFormBuilderDummy).family_name, :family_name
