7ab6daf03d7eff0f15209f480ba514549590067d
Paul McKibbin <pmckibbin@gmail.com>
2018-04-17 18:04:02 +0100

    Set 1.2.1 as default for hei_demo

--- willow/lib/rdss/messaging/generators/generate_header.rb
+++ willow/lib/rdss/messaging/generators/generate_header.rb
@@ -24,7 +24,7 @@ module Rdss
 
         def version_map
           {
-            'current': '2.1.0',
+            'current': '1.2.1',
             '1.2.1': '1.2.1',
             '2.1.0': '2.1.0'
           }
