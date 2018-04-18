e638f4224db45858d2d834a3279cf9924dc3edad
Finlay McCourt <finlay.mccourt@digirati.com>
2018-04-18 11:54:58 +0100

    Adding in a hardcoded JS file to overwrite the default.
    Necessary as the config of this isn't being passed through from
    the variables set in the Hyrax initialiser.

---
+++ willow/app/assets/javascripts/hyrax/config.js
@@ -0,0 +1,8 @@
+Hyrax.config = {
+    uploader: {
+        limitConcurrentUploads: 1, 
+        maxNumberOfFiles: 100, 
+        maxFileSize: 6442450944, // 6GB 
+        sequentialUploads: true
+    }
+}
