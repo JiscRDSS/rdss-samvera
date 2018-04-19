61688efe7be0a263229e74ac0696fbd4456c412c
paul.mak <pmckibbin@gmail.com>
2018-04-19 11:13:39 +0100

    Still in my change list for some reason.

--- willow/app/models/cdm/messaging/file_right.rb
+++ willow/app/models/cdm/messaging/file_right.rb
@@ -1,7 +1,7 @@
 #Endpoint that has the same effective name in the mapping and the model. objectRights maps to :object_rights
 module Cdm
   module Messaging
-    class FileRights < MessageMapper
+    class FileRight < MessageMapper
       def hash_value(_, object)
         {
           rightsStatement: [ 'not yet implemented' ],
