39a8a23094cd8ec50c28bb2140aaa2a708f19648
Paul McKibbin <pmckibbin@gmail.com>
2018-04-24 17:31:57 +0100

    Fix test which didn't compare the title.

--- willow/spec/inputs/facets/select.rb
+++ willow/spec/inputs/facets/select.rb
@@ -1,6 +1,6 @@
-RSpec.shared_examples 'select' do |object, key, value|
+RSpec.shared_examples 'select' do |object, key, value, tooltip|
   let(:select_tag) { 'select' }
-  let(:option_tag) { %Q(<option value="#{key}">#{value}</option>) }
+  let(:option_tag) { tooltip.present? ? %Q(<option title="#{tooltip}" value="#{key}">#{value}</option>) : %Q(<option value="#{key}">#{value}</option>) }
 
   it 'renders a select' do
     expect(object).to include(select_tag)
