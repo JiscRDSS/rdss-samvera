59bdebdc652f7beeb472be985857d0f725dde906
Stewart Campbell <stewartcuk@gmail.com>
2018-04-27 09:55:35 +0200

    Add messaging

--- willow/app/models/cdm/messaging/person_identifier.rb
+++ willow/app/models/cdm/messaging/person_identifier.rb
@@ -2,13 +2,6 @@
 module Cdm
   module Messaging
     class PersonIdentifier < MessageMapper
-      def hash_value(*)
-        {
-          personIdentifierType: 1,
-          personIdentifierValue: 'not yet implemented'
-        }
-      end
-
       def array_value(message_mapper, object)
         [
           hash_value(message_mapper, object)
