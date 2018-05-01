0b28f050b8aa8ddffea948cf727b2745f62c68c4
Paul McKibbin <pmckibbin@gmail.com>
2018-05-01 16:31:56 +0100

    Force it to nil, since the children still appear to be being called with EmptyMapper.

--- willow/app/models/cdm/messaging/file_right.rb
+++ willow/app/models/cdm/messaging/file_right.rb
@@ -1,7 +1,10 @@
 #Endpoint that has the same effective name in the mapping and the model. objectRights maps to :object_rights
 module Cdm
   module Messaging
-    class FileRight < EmptyMapper
+    class FileRight < MessageMapper
+      def hash_value(_, object)
+        nil
+      end
     end
   end
 end
