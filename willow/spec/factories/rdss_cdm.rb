989f6d8bea41f72d9745ef3a5b03c820ad29d27b
Paul McKibbin <pmckibbin@gmail.com>
2018-05-03 15:52:07 +0100

    Fix the missing objects_rights that was throwning an exception on the messaging thread but which wasn't being picked up by the travis tests.

--- willow/spec/factories/rdss_cdm.rb
+++ willow/spec/factories/rdss_cdm.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
 
   factory :rdss_cdm do
     title ["RDSS CDM"]
+    object_rights_attributes [ {rights_statement: ''} ]
     access_control
     skip_create
     override_new_record
