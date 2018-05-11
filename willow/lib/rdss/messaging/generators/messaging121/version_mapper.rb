3d89f6a60b5ca4d4abb0a74669e8b9d9e1e38240
Paul McKibbin <pmckibbin@gmail.com>
2018-05-11 14:54:21 +0100

    Add full namespace around files to allow for autoload to find them properly.

--- willow/lib/rdss/messaging/generators/messaging121/version_mapper.rb
+++ willow/lib/rdss/messaging/generators/messaging121/version_mapper.rb
@@ -1,7 +1,13 @@
-module Messaging121
-  module VersionMapper
-    def default_version
-      '1.2.1'
+module Rdss
+  module Messaging
+    module Generators
+      module Messaging121
+        module VersionMapper
+          def default_version
+            '1.2.1'
+          end
+        end
+      end
     end
   end
 end
